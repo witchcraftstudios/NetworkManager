b80530add47e81da3549da1eacc034c796b55fd6
witchcraftstudios <developer@witchcraftstudios.com>
2017-04-11 15:35:50 +0200

    Update

--- SampleModule/src/main/java/com/network/manager/IntegerManager.java
+++ SampleModule/src/main/java/com/network/manager/IntegerManager.java
@@ -2,15 +2,11 @@ package com.network.manager;
 
 import android.support.annotation.IntDef;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
 public class IntegerManager {
 
     public static final int INT_ONE = 1;
     public static final int INT_TWO = 2;
 
-    @Retention(RetentionPolicy.SOURCE)
     @IntDef({INT_ONE, INT_TWO})
     @interface IntegerMode {
     }
--- SampleModule/src/main/java/com/network/manager/MainActivity.java
+++ SampleModule/src/main/java/com/network/manager/MainActivity.java
@@ -9,10 +9,6 @@ import com.network.library.BackgroundTask;
 import com.network.library.NetworkManager;
 import com.network.library.NetworkManagerCallbacks;
 
-import java.util.Random;
-
-import static com.network.manager.IntegerManager.INT_TWO;
-
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
 
@@ -20,14 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
-
-        String test;
-        if (new Random().nextInt(5) > 2) {
-            test = "";
-        } else {
-            test = null;
-        }
-        IntegerManager.setInteger(INT_TWO, test);
     }
 
     public void onStartClick(View view) {
